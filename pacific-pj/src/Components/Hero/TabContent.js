e60fbbf8a568ea00a0e5520808bc84b62841ed46
Shangri <astasenko@smart-ui.pro>
2021-05-05 13:53:04 +0300

    created tabs animation

--- pacific-pj/src/Components/Hero/TabContent.js
+++ pacific-pj/src/Components/Hero/TabContent.js
@@ -1,47 +1,55 @@
 import React, {useState} from "react";
-import Calendar from 'react-calendar';
+import {CSSTransition} from "react-transition-group";
 
 
-export const TabContent = ({content}) => {
+export const TabContent = ({
+                               content,
+                               runAnimation
+                           }) => {
 
     const [showCalendar, setShowCalendar] = useState(false);
     const [calendarItem, setCalendarItem] = useState(null);
 
-    const handleClickCalendar = (e) => {
-        setShowCalendar(!showCalendar);
-        console.log(showCalendar,  e.currentTarget.id)
-    };
+    // const handleClickCalendar = (e) => {
+    //     setShowCalendar(!showCalendar);
+    //     // console.log(showCalendar,  e.currentTarget.id)
+    // };
 
     return (
         <>
             {
                 content.map((val, i) => {
-
-                    console.log(val.id, i)
                     return (
-                        <li key={i} className='hero__tab-item'>
-                            <h4 className='hero__tab-title'>{val.titleContent}</h4>
-                            <div className="hero__icon">
-                                {
-                                    val.hasOwnProperty('select') ?
-                                        <span className="hero__icon-item">{val.select}</span>
-                                        :
-                                        <>
-                                            <span className="hero__icon-item">{val.icon}
-                                            </span>
-                                            {showCalendar && i === val.id ?'' : ''}
-                                            <input type="search" placeholder={val.placeholder}
-                                                   onClick={handleClickCalendar}/>
-                                        </>
-                                }
-                            </div>
-                        </li>
+                        <CSSTransition
+                            in={runAnimation}
+                            timeout={200}
+                            classNames="my-node">
+                            <li key={i} className='hero__tab-item'>
+                                <h4 className='hero__tab-title'>{val.titleContent}</h4>
+                                <div className="hero__icon">
+                                    {
+                                        val.hasOwnProperty('select') ?
+                                            <span className="hero__icon-item">{val.select}</span>
+                                            :
+                                            <>
+                                            <span className="hero__icon-item">{val.icon}</span>
+                                                <input type="search" placeholder={val.placeholder}/>
+                                            </>
+                                    }
+                                </div>
+                            </li>
+                        </CSSTransition>
                     )
                 })
             }
+            <CSSTransition
+                in={runAnimation}
+                timeout={300}
+                classNames="my-node">
             <li className='hero__tab-item'>
                 <a href='\' className='hero__item-search'>search</a>
             </li>
+            </CSSTransition>
         </>
     )
 }
--- pacific-pj/src/Components/Hero/TabsComponent.js
+++ pacific-pj/src/Components/Hero/TabsComponent.js
@@ -1,22 +1,35 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {TabContent} from './TabContent';
 
 
-export function TabsComponent({ TabsData }) {
-    const [ active, setActive ] = useState(0);
+
+export function TabsComponent({TabsData}) {
+    const [active, setActive] = useState(0);
+    const [runAnimation, setRunAnimation] = useState(false);
+
+
+    const handleClick = (i) => {
+        setActive(i);
+        setRunAnimation(!runAnimation);
+    }
+    useEffect(() => {
+        setRunAnimation(true)
+    }, [runAnimation]);
 
     return (
         <div className="hero__tab-container">
             <ul className="hero__tab">
                 {TabsData.map((value, i) => (
-                    <li key={i+'search'}
+                    <li key={i + 'search'}
                         className={`hero__tab-links ${i === active ? 'active' : ''}`}
-                        onClick={e => {setActive(i)}}
+                        onClick={e => {
+                            handleClick(i)
+                        }}
                         data-index={i}
                     >{value.title}</li>
                 ))}
                 <ul className="hero__tab-content-items">
-                {TabsData[active] && <TabContent  {...TabsData[active]} active={active} />}
+                    {TabsData[active] && <TabContent  {...TabsData[active]} active={active} runAnimation={runAnimation}/>}
                 </ul>
             </ul>
         </div>
